ccb710100e2b101bb153cd0391bce3276c2c718a
Parker <ward.parker26@gmail.com>
2023-04-03 12:14:09 -0600

    ok

--- Keepr.client/src/router.js
+++ Keepr.client/src/router.js
@@ -12,6 +12,11 @@ const routes = [
     component: loadPage('HomePage')
   },
   {
+    path: '/vault/:vaultId',
+    name: 'Vaults',
+    component: loadPage('VaultPage')
+  },
+  {
     path: '/about',
     name: 'About',
     component: loadPage('AboutPage')
