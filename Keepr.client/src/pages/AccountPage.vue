333146c27b98ff2a76adc4a445917c4eb6509186
Parker <ward.parker26@gmail.com>
2023-04-03 12:31:00 -0600

    working on account page

--- Keepr.client/src/pages/AccountPage.vue
+++ Keepr.client/src/pages/AccountPage.vue
@@ -1,8 +1,25 @@
 <template>
   <div class="about text-center">
+    <img class="rounded-circle elevation-3" :src="account.picture" alt="" />
     <h1>Welcome {{ account.name }}</h1>
-    <img class="rounded" :src="account.picture" alt="" />
     <p>{{ account.email }}</p>
+  </div>
+
+  <div class="container">
+    <div class="row">
+      <div class="col-md-8">
+        <h1><b>Vaults</b></h1>
+
+      </div>
+    </div>
+  </div>
+
+  <div class="container">
+    <div class="row">
+      <div class="col-md-8">
+        <h1><b>Keeps</b></h1>
+      </div>
+    </div>
   </div>
 </template>
 
